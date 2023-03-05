6654c3ec3253cbbc82515171734dbabffe209ae1
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-05 11:35:32 +0100

    Definizione del costruttore

--- index.php
+++ index.php
@@ -5,5 +5,13 @@
         public $title;
         public $runningTime;
         public $language;
+
+        // Definisco il costruttore
+        public function __construct($title, $runningTime, $language)
+        {
+            $this->title = $title;
+            $this->runningTime = $runningTime;
+            $this->language = $language;
+        }
     }
 ?>
